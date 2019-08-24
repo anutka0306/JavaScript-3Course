6899be3c9d60da71437291a2aa4226c34bab1ed8
anutka0306 <annakotelnikova4@gmail.com>
2019-08-24 22:05:41 +0300

    start

--- js/main.js
+++ js/main.js
@@ -1,21 +1,48 @@
-const products = [
-  {id: 1, title: 'Notebook', price: 1000},
-  {id: 2, title: 'Mouse', price: 100},
-  {id: 3, title: 'Keyboard', price: 250},
-  {id: 4, title: 'Gamepad', price: 150},
-];
+class ProductList {
+    constructor(container = ".products"){
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._fetchProducts();
+        this._render();
+    }
+    _fetchProducts(){
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 1000},
+            {id: 2, title: 'Mouse', price: 100},
+            {id: 3, title: 'Keyboard', price: 250},
+            {id: 4, title: 'Gamepad', price: 150},
+        ];
+    }
 
-const renderProduct = (title = '', price = 0) => {
-  return `<div class="product-item">
-            <h3>${title}</h3>
-            <p>${price}</p>
+    _render(){
+        const block = document.querySelector(this.container);
+
+        for(let product of this.goods){
+            const productObject = new ProductItem(product);
+            this.allProducts.push(productObject);
+            block.insertAdjacentHTML('beforeend', productObject.render());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product, img ='https://placehold.it/200x150'){
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
+    render(){
+        return `<div class="product-item" data-id = "${this.id}">
+            <img src="${this.img}" alt = "Some img">
+            <div class="desc">
+            <h3>${this.title}</h3>
+            <p>${this.price}</p>
             <button class="by-btn">Добавить</button>
+            </div>
           </div>`;
-};
+    }
+}
 
-const renderProducts = list => {
-  const productList = list.map(item => renderProduct(item.title, item.price));
-  document.querySelector('.products').innerHTML = productList;
-};
-
-renderProducts(products);
+const list = new ProductList();
